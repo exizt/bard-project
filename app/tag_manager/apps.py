dd4ac8a2ec3e0fb05ab6ceb9bec2d0547c6c71b2
exizt <e2xist@gmail.com>
2023-02-13 15:26:34 +0900

    minor

--- app/tag_manager/apps.py
+++ app/tag_manager/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TagManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tag_manager'
+    verbose_name = '2. 태그 관리자'
