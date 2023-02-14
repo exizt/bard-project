e7c8fce95a092a0ff81f205612d46b610f5579fb
exizt <e2xist@gmail.com>
2023-02-15 01:25:39 +0900

    tag support unicode

--- app/tag_manager/urls.py
+++ app/tag_manager/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # path('', views.index),
     path('', views.index, name='index'),
     # path('<slug:tag_slug>/', views.index_by_tag, name='articles_by_tag'),
-    re_path('(?P<tag_slug>[-a-z0-9\%\#]+)/', views.index_by_tag, name='articles_by_tag'),
+    # re_path(r'^(?P<tag_slug>[-a-z0-9\%\#]+)/$', views.index_by_tag, name='articles_by_tag'),
+    re_path(r'^(?P<tag_slug>[-\w]+)/$', views.index_by_tag, name='articles_by_tag'),
 ]
